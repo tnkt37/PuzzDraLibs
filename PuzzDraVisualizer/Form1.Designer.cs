fda318e70f6fd4c87d80ef2f09f4f7a81e169d78
tnkt37 <tnkt37@outlook.jp>
2014-10-06 19:35:19 +0900

    Add Visuallizer

---
+++ PuzzDraVisualizer/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace PuzzDraVisualizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainPictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.logTextBox = new System.Windows.Forms.TextBox();
+            this.beamWeightText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.operatedTimeText = new System.Windows.Forms.TextBox();
+            this.waitedTimeText = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.fallDoropCheckBox = new System.Windows.Forms.CheckBox();
+            this.moveFinishButton = new System.Windows.Forms.Button();
+            this.randomizeButton = new System.Windows.Forms.Button();
+            this.autoMovementButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mainPictureBox
+            // 
+            this.mainPictureBox.Location = new System.Drawing.Point(12, 12);
+            this.mainPictureBox.Name = "mainPictureBox";
+            this.mainPictureBox.Size = new System.Drawing.Size(310, 260);
+            this.mainPictureBox.TabIndex = 0;
+            this.mainPictureBox.TabStop = false;
+            this.mainPictureBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.mainPictureBox_MouseUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(371, 347);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(236, 60);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "操作説明：\r\n右クリックでドロップ切り替え\r\n左クリックでドロップ選択\r\n矢印キーで選択したドロップを移動\r\n移動終了ボタン/別のドロップを選択でコンボ評価";
+            // 
+            // logTextBox
+            // 
+            this.logTextBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.logTextBox.Location = new System.Drawing.Point(373, 12);
+            this.logTextBox.Multiline = true;
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.ReadOnly = true;
+            this.logTextBox.Size = new System.Drawing.Size(297, 322);
+            this.logTextBox.TabIndex = 6;
+            this.logTextBox.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.logTextBox_PreviewKeyDown);
+            // 
+            // beamWeightText
+            // 
+            this.beamWeightText.Location = new System.Drawing.Point(12, 297);
+            this.beamWeightText.Name = "beamWeightText";
+            this.beamWeightText.Size = new System.Drawing.Size(100, 19);
+            this.beamWeightText.TabIndex = 7;
+            this.beamWeightText.Text = "300";
+            this.beamWeightText.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.beamWeightText_PreviewKeyDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 279);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 12);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "ビーム幅";
+            // 
+            // operatedTimeText
+            // 
+            this.operatedTimeText.Location = new System.Drawing.Point(10, 340);
+            this.operatedTimeText.Name = "operatedTimeText";
+            this.operatedTimeText.Size = new System.Drawing.Size(100, 19);
+            this.operatedTimeText.TabIndex = 9;
+            this.operatedTimeText.Text = "100";
+            this.operatedTimeText.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.beamWeightText_PreviewKeyDown);
+            // 
+            // waitedTimeText
+            // 
+            this.waitedTimeText.Location = new System.Drawing.Point(10, 391);
+            this.waitedTimeText.Name = "waitedTimeText";
+            this.waitedTimeText.Size = new System.Drawing.Size(100, 19);
+            this.waitedTimeText.TabIndex = 10;
+            this.waitedTimeText.Text = "800";
+            this.waitedTimeText.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.beamWeightText_PreviewKeyDown);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 322);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 12);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "操作待ち時間(ミリ秒)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 373);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 12);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "アニメーション待ち時間(ミリ秒)";
+            // 
+            // fallDoropCheckBox
+            // 
+            this.fallDoropCheckBox.AutoSize = true;
+            this.fallDoropCheckBox.Location = new System.Drawing.Point(177, 391);
+            this.fallDoropCheckBox.Name = "fallDoropCheckBox";
+            this.fallDoropCheckBox.Size = new System.Drawing.Size(145, 16);
+            this.fallDoropCheckBox.TabIndex = 13;
+            this.fallDoropCheckBox.Text = "コンボ中にドロップを落とす";
+            this.fallDoropCheckBox.UseVisualStyleBackColor = true;
+            this.fallDoropCheckBox.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.logTextBox_PreviewKeyDown);
+            // 
+            // moveFinishButton
+            // 
+            this.moveFinishButton.Location = new System.Drawing.Point(177, 279);
+            this.moveFinishButton.Name = "moveFinishButton";
+            this.moveFinishButton.Size = new System.Drawing.Size(143, 23);
+            this.moveFinishButton.TabIndex = 14;
+            this.moveFinishButton.Text = "移動完了";
+            this.moveFinishButton.UseVisualStyleBackColor = true;
+            this.moveFinishButton.MouseDown += new System.Windows.Forms.MouseEventHandler(this.moveFinishButton_MouseClick);
+            this.moveFinishButton.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.beamWeightText_PreviewKeyDown);
+            // 
+            // randomizeButton
+            // 
+            this.randomizeButton.Location = new System.Drawing.Point(177, 317);
+            this.randomizeButton.Name = "randomizeButton";
+            this.randomizeButton.Size = new System.Drawing.Size(143, 23);
+            this.randomizeButton.TabIndex = 15;
+            this.randomizeButton.Text = "盤面のランダム生成";
+            this.randomizeButton.UseVisualStyleBackColor = true;
+            this.randomizeButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.randomizeButton_MouseClick);
+            this.randomizeButton.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.beamWeightText_PreviewKeyDown);
+            // 
+            // autoMovementButton
+            // 
+            this.autoMovementButton.Location = new System.Drawing.Point(177, 354);
+            this.autoMovementButton.Name = "autoMovementButton";
+            this.autoMovementButton.Size = new System.Drawing.Size(143, 23);
+            this.autoMovementButton.TabIndex = 16;
+            this.autoMovementButton.Text = "コンボ解析＆自動操作";
+            this.autoMovementButton.UseVisualStyleBackColor = true;
+            this.autoMovementButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.autoMovementButton_MouseClick);
+            this.autoMovementButton.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.beamWeightText_PreviewKeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 422);
+            this.Controls.Add(this.autoMovementButton);
+            this.Controls.Add(this.randomizeButton);
+            this.Controls.Add(this.moveFinishButton);
+            this.Controls.Add(this.fallDoropCheckBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.waitedTimeText);
+            this.Controls.Add(this.operatedTimeText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.beamWeightText);
+            this.Controls.Add(this.logTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.mainPictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "PuzzDraVisualizer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.mainPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox mainPictureBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox logTextBox;
+        private System.Windows.Forms.TextBox beamWeightText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox operatedTimeText;
+        private System.Windows.Forms.TextBox waitedTimeText;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox fallDoropCheckBox;
+        private System.Windows.Forms.Button moveFinishButton;
+        private System.Windows.Forms.Button randomizeButton;
+        private System.Windows.Forms.Button autoMovementButton;
+    }
+}
---
+++ PuzzDraVisualizer/Form1.cs
@@ -0,0 +1,386 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using PuzzDraAnalyzer;
+
+namespace PuzzDraVisualizer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Color[] backColors = { Color.FromArgb(30, 17, 11), Color.FromArgb(73, 42, 35) };
+        Bitmap[] dropImages;
+
+        int[] board;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            logTextBox.Enabled = false;
+            mainPictureBox.Focus();
+            PuzzDraPInvoke.Initialize();
+            dropImages = new[]
+                {
+                    new Bitmap(size, size),
+                    new Bitmap("火ドロップ.png"),
+                    new Bitmap("水ドロップ.png"),
+                    new Bitmap("木ドロップ.png"),
+                    new Bitmap("光ドロップ.png"),
+                    new Bitmap("闇ドロップ.png"),
+                    new Bitmap("ハートドロップ.png"),
+                };
+
+            //初期はランダム生成
+            board = GenerateRandomBoard();
+
+            //描画
+            DrawBoard();
+        }
+
+        static Random random = new Random();
+        int[] GenerateRandomBoard()
+        {
+            const int Width = 6;
+            const int Height = 5;
+            var board = new int[30];
+            for (int i = 0; i < board.Length; i++)
+            {
+                int drop = 0;
+                int r = i / Width, c = i % Width;
+                while (true)
+                {
+                    drop = random.Next(6) + 1;
+                    if (c - 2 >= 0 && drop == board[i - 1] && drop == board[i - 2])
+                        continue;
+                    if (r - 2 >= 0 && drop == board[i - Width] && drop == board[i - Width * 2])
+                        continue;
+
+                    break;
+                }
+                board[i] = drop;
+            }
+            return board;
+        }
+
+        void ChangeBoard(int row, int column, int attr)
+        {
+            board[ToIndex(row, column)] = attr;
+        }
+
+        int GetBoardData(int row, int column)
+        {
+            return board[ToIndex(row, column)];
+        }
+
+        int[] dr = { -1, 0, 1, 0 };
+        int[] dc = { 0, 1, 0, -1 };
+        void SwapDrop(int row, int column, int direction, bool selectedDrop = false)
+        {
+            int nr = row + dr[direction];
+            int nc = column + dc[direction];
+            if (OutOfRange(nr, nc)) return;
+
+            int index = ToIndex(row, column);
+            int ni = ToIndex(nr, nc);
+
+            int tmp = board[index];
+            board[index] = board[ni];
+            board[ni] = tmp;
+
+            if (selectedDrop)
+            {
+                selectedRow = nr;
+                selectedColumn = nc;
+            }
+        }
+
+        int selectedRow = -1, selectedColumn = -1;
+
+        int ToIndex(int row, int column)
+        {
+            return row * Columns + column;
+        }
+
+        const int size = 50;
+        const int Columns = 6;
+        const int Rows = 5;
+        void DrawBoard()
+        {
+            var image = new Bitmap(mainPictureBox.Width, mainPictureBox.Height);
+            using (var gr = Graphics.FromImage(image))
+            {
+                for (int i = 0; i < Rows; i++)
+                {
+                    for (int j = 0; j < Columns; j++)
+                    {
+                        var back = backColors[(ToIndex(i, j) + (i % 2)) % backColors.Length];
+                        gr.FillRectangle(new SolidBrush(back), j * size, i * size, size, size);
+                        gr.DrawImage(dropImages[GetBoardData(i, j)], j * size, i * size, size, size);
+                    }
+                }
+
+                if (!OutOfRange(selectedRow, selectedColumn))
+                {
+                    var pen = new Pen(Color.GhostWhite);
+                    pen.Width = 3;
+                    gr.DrawEllipse(pen, selectedColumn * size - 1, selectedRow * size - 1, size + 1, size + 1);
+                }
+            }
+
+
+            mainPictureBox.Image = image;
+        }
+
+        bool IsSelecting()
+        {
+            return !OutOfRange(selectedRow, selectedColumn);
+        }
+
+        void UnSelect()
+        {
+            selectedRow = -1;
+            selectedColumn = -1;
+        }
+
+        bool OutOfRange(int row, int column)
+        {
+            return row < 0 || row >= Rows || column < 0 || column >= Columns;
+        }
+
+        void ChangeToNextDrop(int row, int column)
+        {
+            if (OutOfRange(row, column)) return;
+            ChangeBoard(row, column, (GetBoardData(row, column) + 1) % dropImages.Length);
+            DrawBoard();
+        }
+
+        void SelectDrop(int row, int column)
+        {
+            selectedRow = row;
+            selectedColumn = column;
+            DrawBoard();
+        }
+
+        //この後矢印の描画をやりたかった
+        //async Task CalcRoute()
+        //{
+        //    var tup = PuzzDraPInvoke.BeamSerach(board);
+        //    int score = tup.Item2;
+        //    var route = tup.Item3;
+        //    int startRow = tup.Item4;
+        //    int startColumn = tup.Item5;
+
+        //    await MoveDropByRoute(startRow, startColumn, route);
+        //    await ShowCombo();
+        //}
+
+        bool isAnimating = false;
+
+        async Task CalcAutoMovemnt()
+        {
+            if (isAnimating) return;
+            
+            int beamWeight;
+            beamWeight = int.TryParse(beamWeightText.Text, out beamWeight)
+                ? Math.Max(beamWeight, 1)
+                : 50;
+
+            WriteLineLog();
+            WriteLineLog("ルート解析開始");
+            WriteLineLog("ビーム幅: " + beamWeight);
+            await Task.Delay(10);
+            isAnimating = true;
+            var startTime = DateTime.Now;
+            var tup = await PuzzDraPInvoke.BeamSerach(board, beamWeight);
+            var elapsed = DateTime.Now - startTime;
+            WriteLineLog("ルート解析完了");
+            WriteLineLog("解析時間:" + elapsed.TotalSeconds + "秒");
+
+            int score = tup.Item2;
+            var route = tup.Item3;
+            int startRow = tup.Item4;
+            int startColumn = tup.Item5;
+            WriteLineLog("開始地点 (上, 左) = (" + startRow + ", " + startColumn + ")");
+            await MoveDropByRoute(startRow, startColumn, route);
+            await ShowCombo();
+            WriteLineLog();
+            isAnimating = false;
+        }
+
+        int waitedTime = 800;
+        int operatedTime = 100;
+
+        char[] arrows = { '↑', '→', '↓', '←' };
+        async Task MoveDropByRoute(int startRow, int startColumn, int[] route)
+        {
+            operatedTime = int.TryParse(operatedTimeText.Text, out operatedTime)
+                ? Math.Max(30, operatedTime)
+                : 500;
+            SelectDrop(startRow, startColumn);
+            WriteLineLog("自動操作開始");
+            int size = 0;
+            for (int i = 0; i < route.Length && route[i] != -1; i++)
+            {
+                SwapDrop(selectedRow, selectedColumn, route[i], true);
+                DrawBoard();
+                logTextBox.Text += arrows[route[i]];
+                ScrollToEndLog();
+                size++;
+                await Task.Delay(operatedTime);
+            }
+            WriteLineLog();
+            UnSelect();
+            WriteLineLog("自動操作完了");
+            WriteLineLog("ルート長 " + size);
+        }
+
+        void WriteLineLog(object text = null, bool returnLine = true)
+        {
+            text = text ?? "";
+            logTextBox.Text += text.ToString() + (returnLine ? "\r\n" : "");
+            ScrollToEndLog();
+        }
+
+        void ScrollToEndLog()
+        {
+            logTextBox.SelectionStart = logTextBox.TextLength;
+            logTextBox.ScrollToCaret();
+        }
+
+
+        async Task ShowCombo()
+        {
+            waitedTime = int.TryParse(waitedTimeText.Text, out waitedTime)
+                ? Math.Max(30, waitedTime)
+                : 800;
+            var isFalenDrop = fallDoropCheckBox.Checked;
+
+            UnSelect();
+            WriteLineLog();
+            WriteLineLog("コンボ処理開始");
+            int comboNum = 0;
+            while (true)
+            {
+                var combo = PuzzDraPInvoke.EvalCombo(board);
+                DrawBoard();
+                comboNum += combo;
+                if (combo >= 1)
+                {
+                    WriteLineLog("+" + combo + "コンボ");
+                }
+                await Task.Delay(waitedTime);
+                if (combo == 0) break;
+                var pushed = PuzzDraPInvoke.PushDownDrops(board);
+                DrawBoard();
+                await Task.Delay(waitedTime);
+
+                if (isFalenDrop)
+                {
+                    PuzzDraPInvoke.FillBoardRandom(board);
+                }
+
+                DrawBoard();
+                await Task.Delay(waitedTime);
+            }
+            WriteLineLog("合計" + comboNum + "コンボ");
+            WriteLineLog("コンボ処理完了");
+        }
+
+        private async void mainPictureBox_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (isAnimating) return;
+            int column = e.X / size;
+            int row = e.Y / size;
+            if (e.Button == System.Windows.Forms.MouseButtons.Right)
+            {
+                ChangeToNextDrop(row, column);
+
+                return;
+            }
+
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+            {
+                if (!IsSelecting() || (selectedRow == row && selectedColumn == column))
+                {
+                    SelectDrop(row, column);
+                    return;
+                }
+                else
+                {
+                    await ShowCombo();
+                    WriteLineLog();
+                    UnSelect();
+                }
+            }
+        }
+
+        private void Form1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (isAnimating || !IsSelecting()) return;
+
+            int direction =
+                  e.KeyCode == Keys.Up
+                    ? 0
+                : e.KeyCode == Keys.Right
+                    ? 1
+                : e.KeyCode == Keys.Down
+                    ? 2
+                : e.KeyCode == Keys.Left
+                    ? 3
+                : -1;
+
+            if (direction == -1) return;
+
+            moveFinishButton.Focus();
+
+            SwapDrop(selectedRow, selectedColumn, direction, true);
+            var arr = new[] { '上', '右', '下', '左' };
+            WriteLineLog(arr[direction], false);
+            ScrollToEndLog();
+            DrawBoard();
+        }
+
+        private void logTextBox_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            Form1_PreviewKeyDown(this, e);
+        }
+
+        private void beamWeightText_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            Form1_PreviewKeyDown(this, e);
+        }
+
+        private async void moveFinishButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (isAnimating) return;
+            isAnimating = true;
+            await ShowCombo();
+            WriteLineLog();
+            UnSelect();
+            isAnimating = false;
+        }
+
+        private void randomizeButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (isAnimating) return;
+            board = GenerateRandomBoard();
+            DrawBoard();
+        }
+
+        private async void autoMovementButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            await CalcAutoMovemnt();
+        }
+    }
+}
